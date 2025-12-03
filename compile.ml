d13ec13644ebb79fa8f8e8b4cb8a2a28f861eb43
Quentin Riggi <78906218+UentR@users.noreply.github.com>
2025-12-03 17:41:31 +0100

    Need human

--- compile.ml
+++ compile.ml
@@ -19,7 +19,7 @@ module StringMap = Map.Make(String)
 
 type struct_layout = {
   size: int;
-  offsets: (int * typ) StringMap.t; 
+  offsets: (int * typ) StringMap.t;
 }
 
 let struct_table = ref StringMap.empty
@@ -30,13 +30,12 @@ let compute_struct_layout s =
       (StringMap.add id.id (offset, t) map, offset + 4)
     ) (StringMap.empty, 0) s.fields
   in
-  { size = total_size;
-  offsets = offsets }
+  { size = total_size; offsets = offsets }
 
 (* Gestion Fonctions *)
 type func_sig = { 
   nargs: int;
-  rets: typ list; 
+  rets: typ list;
 }
 let func_table = ref StringMap.empty
 
@@ -44,7 +43,7 @@ let get_func_sig id =
   try StringMap.find id !func_table
   with Not_found -> 
     Printf.eprintf "Fatal error: Fonction inconnue '%s'\n" id;
-  exit 2
+    exit 2
 
 
 type env = {
@@ -61,7 +60,7 @@ let empty_env = {
   ret_ptr_offsets = [] 
 }
 
-(* Inférence type*)
+(* Inférence type *)
 let rec get_expr_type env e = match e.edesc with
   | Int _ -> [TInt]
   | Bool _ -> [TBool]
@@ -105,35 +104,40 @@ let compile_struct_printers () =
     let str_nil = add_string "<nil>" in
     let str_space = add_string " " in
     
-    let print_code = 
+    (* On génère le code pour afficher chaque champ *)
+    let rec print_fields = function
+      | [] -> nop
+      | (_, (off, t)) :: rest ->
+           let load = lw a0 off s0 in
+           let print_val = match t with
+             | TString -> li v0 4 @@ syscall
+             | TStruct _ -> li v0 1 @@ syscall 
+             | _ -> li v0 1 @@ syscall
+           in
+           let sep = if rest <> [] then la a0 str_space @@ li v0 4 @@ syscall else nop in
+           
+           load @@ print_val @@ sep @@ print_fields rest
+    in
+
+    let body = 
         label print_label
         @@ beqz a0 nil_label
         @@ addi sp sp (-4) @@ sw s0 0(sp)
         @@ move s0 a0
         @@ la a0 str_start @@ li v0 4 @@ syscall
-        @@ (
-          let rec loop = function
-            | [] -> nop
-            | (_, (off, t)) :: rest ->
-                 lw a0 off s0
-                 @@ (match t with
-                     | TString -> li v0 4 @@ syscall
-                     | TStruct _ -> 
-                        li v0 1 @@ syscall 
-                     | _ -> li v0 1 @@ syscall
-                    )
-                 @@ (if rest <> [] then la a0 str_space @@ li v0 4 @@ syscall else nop)
-                 @@ loop rest
-          in loop fields_sorted
-        )
+        @@ print_fields fields_sorted
         @@ la a0 str_end @@ li v0 4 @@ syscall
         @@ lw s0 0(sp) @@ addi sp sp 4
         @@ jr ra
-        @@ label nil_label
+    in
+    
+    let nil_case = 
+        label nil_label
         @@ la a0 str_nil @@ li v0 4 @@ syscall
         @@ jr ra
     in
-    code @@ print_code
+    
+    code @@ body @@ nil_case
   ) !struct_table nop
 
 
@@ -154,69 +158,138 @@ let rec tr_addr env e = match e.edesc with
           | _ -> exit 2)
   | _ -> Printf.eprintf "Fatal error: L-value attendue\n"; exit 2
 
+(* let rec tr_addr env e = match e.edesc with
+  | Var id ->
+      (try
+        let (offset, _) = StringMap.find id.id env.vars in
+        addi t0 fp offset
+       with Not_found -> exit 2)
+  | Dot (e_struct, id) ->
+      let base_addr = tr_expr env e_struct in
+      match get_expr_type env e_struct with
+          | [TStruct s_name] -> 
+             let layout = StringMap.find s_name !struct_table in
+             let (off, _) = StringMap.find id.id layout.offsets in
+             base_addr @@ addi t0 t0 off
+          | _ -> exit 2
+  | _ -> Printf.eprintf "Fatal error: L-value attendue\n"; exit 2 *)
+
 and tr_expr env e = match e.edesc with
   | Int(n)  -> li t0 (Int64.to_int n)
   | Bool(b) -> li t0 (if b then 1 else 0)
   | Nil     -> li t0 0
   | String(s) -> let l = add_string s in la t0 l 
   | Var _ -> tr_addr env e @@ lw t0 0 t0
+  
   | New s_name ->
       let layout = try StringMap.find s_name !struct_table with Not_found -> exit 2 in
       let loop_lbl = new_label() in
       let end_lbl = new_label() in
-      li a0 layout.size @@ li v0 9 @@ syscall @@ move t0 v0
-      @@ li t1 0 @@ label loop_lbl @@ li t2 layout.size @@ bge t1 t2 end_lbl
-      @@ add t2 t0 t1 @@ sw zero 0 t2 @@ addi t1 t1 4 @@ j loop_lbl @@ label end_lbl
+      
+      let alloc = li a0 layout.size @@ li v0 9 @@ syscall @@ move t0 v0 in
+      let init_loop = 
+        li t1 0 
+        @@ label loop_lbl 
+        @@ li t2 layout.size 
+        @@ bge t1 t2 end_lbl
+        @@ add t2 t0 t1 
+        @@ sw zero 0 t2 
+        @@ addi t1 t1 4 
+        @@ j loop_lbl 
+        @@ label end_lbl
+      in
+      alloc @@ init_loop
+
   | Dot _ -> tr_addr env e @@ lw t0 0 t0
+  
   | Call (f, args) ->
       let sig_f = get_func_sig f.id in
       if List.length sig_f.rets > 1 then (Printf.eprintf "Call error\n"; exit 2);
-      let push_args = List.fold_left (fun acc arg -> acc @@ tr_expr env arg @@ push t0) nop args in
-      push_args @@ jal f.id @@ addi sp sp (4 * List.length args) @@ move t0 v0
+      
+      let push_args = List.fold_left (fun acc arg -> 
+        acc @@ tr_expr env arg @@ push t0
+      ) nop args in
+      
+      push_args 
+      @@ jal f.id 
+      @@ addi sp sp (4 * List.length args) 
+      @@ move t0 v0
+
   | Unop(Opp, e) -> tr_expr env e @@ sub t0 zero t0
   | Unop(Not, e) -> tr_expr env e @@ seq t0 t0 zero
+  
   | Binop(bop, e1, e2) ->
       let op = match bop with
         | Add -> add | Sub -> sub | Mul -> mul | Div -> div | Rem -> rem
         | Lt -> slt | Le -> sle | Gt -> sgt | Ge -> sge | Eq -> seq | Neq -> sne
         | And -> and_ | Or -> or_ 
       in
-      tr_expr env e2 @@ push t0 @@ tr_expr env e1 @@ pop t1 @@ op t0 t0 t1
+      let c2 = tr_expr env e2 in
+      let c1 = tr_expr env e1 in
+      
+      c2 @@ push t0 @@ c1 @@ pop t1 @@ op t0 t0 t1
+
   | Print(exps) -> 
       let print_space = li a0 32 @@ li v0 11 @@ syscall in
+      
       let compile_print_single e = 
         match e.edesc with
         | Call(f, args) when List.length (get_func_sig f.id).rets > 1 ->
+            (* Cas spécial : print d'une fonction à retour multiple *)
             let sig_f = get_func_sig f.id in
             let rets = sig_f.rets in
             let n_rets = List.length rets in
             let n_args = List.length args in
-            let alloc_res = let rec loop i = if i = 0 then nop else (push zero) @@ loop (i-1) in loop n_rets in
+            
+            (* Allocation place résultats *)
+            let alloc_res = 
+              let rec loop i = if i = 0 then nop else (push zero) @@ loop (i-1) in 
+              loop n_rets 
+            in
+            
             let push_args_code = List.fold_left (fun acc arg -> acc @@ tr_expr env arg @@ push t0) nop args in
+            
+            (* Passage pointeurs résultats *)
             let push_ptrs = 
-              let rec loop i = if i >= n_rets then nop else 
+              let rec loop i = 
+                 if i >= n_rets then nop else 
                  let off = 4 * n_args + 4 * (n_rets - 1 - i) + (i * 4) in
-                 addi t0 sp off @@ push t0 @@ loop (i + 1) in loop 0 in
+                 addi t0 sp off @@ push t0 @@ loop (i + 1) 
+              in loop 0 
+            in
+            
             let call_code = jal f.id in
             let cleanup = addi sp sp (4 * (n_args + n_rets)) in
+            
             let print_code = 
-              let rec loop i = if i >= n_rets then nop else
+              let rec loop i = 
+                 if i >= n_rets then nop else
                  let off = 4 * (n_rets - 1 - i) in
                  let t = List.nth rets i in
-                 lw t0 off sp @@ 
-                 (match t with 
-                  | TString -> move a0 t0 @@ li v0 4 @@ syscall 
-                  | TStruct s -> move a0 t0 @@ jal ("print_struct_" ^ s)
-                  | _ -> move a0 t0 @@ li v0 1 @@ syscall)
-                 @@ (if i < n_rets - 1 then print_space else nop) @@ loop (i+1) in loop 0 in
+                 let load = lw t0 off sp in
+                 let print_val = match t with 
+                    | TString -> move a0 t0 @@ li v0 4 @@ syscall 
+                    | TStruct s -> move a0 t0 @@ jal ("print_struct_" ^ s)
+                    | _ -> move a0 t0 @@ li v0 1 @@ syscall
+                 in
+                 let space = if i < n_rets - 1 then print_space else nop in
+                 load @@ print_val @@ space @@ loop (i+1) 
+              in loop 0 
+            in
+            
             alloc_res @@ push_args_code @@ push_ptrs @@ call_code @@ cleanup @@ print_code @@ addi sp sp (4 * n_rets)
+            
         | _ ->
+            let code_expr = tr_expr env e in
             let type_list = get_expr_type env e in
-            match type_list with
-            | [TString] -> tr_expr env e @@ move a0 t0 @@ li v0 4 @@ syscall
-            | [TStruct s] -> tr_expr env e @@ move a0 t0 @@ jal ("print_struct_" ^ s)
-            | _ -> tr_expr env e @@ move a0 t0 @@ li v0 1 @@ syscall
+            let print_instruction = match type_list with
+                | [TString] -> move a0 t0 @@ li v0 4 @@ syscall
+                | [TStruct s] -> move a0 t0 @@ jal ("print_struct_" ^ s)
+                | _ -> move a0 t0 @@ li v0 1 @@ syscall
+            in
+            code_expr @@ print_instruction
       in
+      
       let rec print_args = function
         | [] -> nop
         | [e] -> compile_print_single e
@@ -233,38 +306,88 @@ let rec tr_seq env = function
 
 and tr_instr env i = match i.idesc with 
   | Expr e -> tr_expr env e
+  
   | Set (lvl, el) ->
       (match el with
        | [{edesc = Call(f, args); _}] when List.length (get_func_sig f.id).rets > 1 ->
+           (* Affectation multiple via appel de fonction *)
            let push_args = List.fold_left (fun acc arg -> acc @@ tr_expr env arg @@ push t0) nop args in
+           
            let push_dst = List.fold_left (fun acc lv -> 
-               match lv.edesc with Var {id="_";_} -> acc @@ addi sp sp (-4) @@ move t0 sp @@ push t0 
-               | _ -> acc @@ tr_addr env lv @@ push t0) nop lvl in
+               match lv.edesc with 
+               | Var {id="_";_} -> 
+                   acc @@ addi sp sp (-4) @@ move t0 sp @@ push t0 
+               | _ -> 
+                   acc @@ tr_addr env lv @@ push t0
+           ) nop lvl in
+           
            push_args @@ push_dst @@ jal f.id @@ addi sp sp (4 * (List.length args + List.length lvl))
+           
        | _ -> 
-         let push_rhs = List.fold_left (fun code e -> code @@ tr_expr env e @@ push t0) nop el in
-         let pop_assign = List.fold_right (fun lv code -> code @@ tr_addr env lv @@ pop t1 @@ sw t1 0 t0) lvl nop in
+         (* Affectation classique *)
+         let push_rhs = List.fold_left (fun code e -> 
+             code @@ tr_expr env e @@ push t0
+         ) nop el in
+         
+         let pop_assign = List.fold_right (fun lv code -> 
+             code @@ tr_addr env lv @@ pop t1 @@ sw t1 0 t0
+         ) lvl nop in
+         
          push_rhs @@ pop_assign)
+
   | Vars (ids, t_opt, seq_body) ->
-      let types = match t_opt with Some t -> List.map (fun _ -> t) ids
-        | None -> match seq_body with {idesc=Set(_, el);_}::_ -> (match el with [{edesc=Call(f,_);_}] -> (get_func_sig f.id).rets | _ -> List.flatten (List.map (get_expr_type env) el)) | _ -> List.map (fun _ -> TInt) ids in
-      let new_env, alloc = List.fold_left2 (fun (e, code) id t -> let off = e.next_local in ({e with vars=StringMap.add id.id (off, t) e.vars; next_local=off-4}, code @@ li t0 0 @@ push t0)) (env, nop) ids types in
+      let types = match t_opt with 
+        | Some t -> List.map (fun _ -> t) ids
+        | None -> 
+            match seq_body with 
+            | {idesc=Set(_, el);_}::_ -> 
+                (match el with 
+                 | [{edesc=Call(f,_);_}] -> (get_func_sig f.id).rets 
+                 | _ -> List.flatten (List.map (get_expr_type env) el)) 
+            | _ -> List.map (fun _ -> TInt) ids 
+      in
+      
+      let new_env, alloc = List.fold_left2 (fun (e, code) id t -> 
+          let off = e.next_local in 
+          let new_vars = StringMap.add id.id (off, t) e.vars in
+          let next = off - 4 in
+          ({e with vars=new_vars; next_local=next}, code @@ li t0 0 @@ push t0)
+      ) (env, nop) ids types in
+      
       alloc @@ tr_seq new_env seq_body
+
   | If(c, s1, s2) ->
-      let l1 = new_label() in let l2 = new_label() in
-      tr_expr env c @@ bnez t0 l1 @@ tr_seq env s2 @@ j l2 @@ label l1 @@ tr_seq env s1 @@ label l2
+      let l1 = new_label() in 
+      let l2 = new_label() in
+      let code_cond = tr_expr env c in
+      code_cond @@ bnez t0 l1 @@ tr_seq env s2 @@ j l2 @@ label l1 @@ tr_seq env s1 @@ label l2
+      
   | For(c, s) ->
-      let l1 = new_label() in let l2 = new_label() in
+      let l1 = new_label() in 
+      let l2 = new_label() in
       j l1 @@ label l2 @@ tr_seq env s @@ label l1 @@ tr_expr env c @@ bnez t0 l2
+      
   | Block s -> tr_seq env s
+  
   | Return el ->
-      if env.ret_ptr_offsets <> [] then (match el with
+      if env.ret_ptr_offsets <> [] then 
+        (match el with
         | [{edesc=Call(f, args);_}] when List.length (get_func_sig f.id).rets > 1 ->
             let push_args = List.fold_left (fun acc arg -> acc @@ tr_expr env arg @@ push t0) nop args in
             let pass_ptrs = List.fold_left (fun acc off -> acc @@ lw t0 off fp @@ push t0) nop env.ret_ptr_offsets in
             push_args @@ pass_ptrs @@ jal f.id @@ addi sp sp (4 * (List.length args + List.length env.ret_ptr_offsets)) @@ j env.exit_label
-        | _ -> List.fold_left2 (fun acc expr off -> acc @@ tr_expr env expr @@ lw t1 off fp @@ sw t0 0 t1) nop el env.ret_ptr_offsets @@ j env.exit_label)
-      else (match el with [] -> j env.exit_label | [e] -> tr_expr env e @@ move v0 t0 @@ j env.exit_label | _ -> exit 2)
+            
+        | _ -> 
+            let store_returns = List.fold_left2 (fun acc expr off -> 
+                acc @@ tr_expr env expr @@ lw t1 off fp @@ sw t0 0 t1
+            ) nop el env.ret_ptr_offsets in
+            store_returns @@ j env.exit_label)
+      else 
+        (match el with 
+         | [] -> j env.exit_label 
+         | [e] -> tr_expr env e @@ move v0 t0 @@ j env.exit_label 
+         | _ -> exit 2)
+
   | Inc e -> tr_addr env e @@ push t0 @@ lw t0 0 t0 @@ addi t0 t0 1 @@ pop t1 @@ sw t0 0 t1
   | Dec e -> tr_addr env e @@ push t0 @@ lw t0 0 t0 @@ addi t0 t0 (-1) @@ pop t1 @@ sw t0 0 t1
 
@@ -276,8 +399,17 @@ let tr_fun df =
   let exit_lbl = "exit_" ^ df.fname.id in
   let n_imp = if List.length sig_f.rets > 1 then List.length sig_f.rets else 0 in
   let tot = List.length df.params + n_imp in
-  let env_p, _ = List.fold_left (fun (e, i) (id, t) -> let off = 8 + 4*(tot-1-i) in ({e with vars=StringMap.add id.id (off, t) e.vars}, i+1)) ({empty_env with exit_label=exit_lbl}, 0) df.params in
-  let env = if n_imp > 0 then let rec loop i acc = if i>=n_imp then (List.rev acc) else loop (i+1) ((8 + 4*(n_imp-1-i))::acc) in {env_p with ret_ptr_offsets=loop 0 []} else env_p in
+  
+  let env_p, _ = List.fold_left (fun (e, i) (id, t) -> 
+      let off = 8 + 4*(tot-1-i) in 
+      ({e with vars=StringMap.add id.id (off, t) e.vars}, i+1)
+  ) ({empty_env with exit_label=exit_lbl}, 0) df.params in
+  
+  let env = if n_imp > 0 then 
+      let rec loop i acc = if i>=n_imp then (List.rev acc) else loop (i+1) ((8 + 4*(n_imp-1-i))::acc) in 
+      {env_p with ret_ptr_offsets=loop 0 []} 
+  else env_p in
+  
   label df.fname.id @@ push ra @@ push fp @@ move fp sp @@ tr_seq env df.body @@ label exit_lbl @@ move sp fp @@ pop fp @@ pop ra @@ jr ra            
 
 let tr_main df =
@@ -285,8 +417,19 @@ let tr_main df =
   label "main" @@ move fp sp @@ tr_seq env df.body @@ label "main_exit" @@ li v0 10 @@ syscall
 
 let tr_prog decls =
-  List.iter (function Struct s -> struct_table := StringMap.add s.sname.id (compute_struct_layout s) !struct_table | Fun f -> func_table := StringMap.add f.fname.id {nargs=List.length f.params; rets=f.return} !func_table) decls;
-  let text = List.fold_left (fun code decl -> match decl with Fun f when f.fname.id="main" -> code @@ tr_main f | Fun f -> code @@ tr_fun f | _ -> code) nop decls in
-  let printers = compile_struct_printers () in (* Ajout des printers *)
+  List.iter (function 
+    | Struct s -> struct_table := StringMap.add s.sname.id (compute_struct_layout s) !struct_table 
+    | Fun f -> func_table := StringMap.add f.fname.id {nargs=List.length f.params; rets=f.return} !func_table
+  ) decls;
+  
+  let text = List.fold_left (fun code decl -> 
+      match decl with 
+      | Fun f when f.fname.id="main" -> code @@ tr_main f 
+      | Fun f -> code @@ tr_fun f 
+      | _ -> code
+  ) nop decls in
+  
+  let printers = compile_struct_printers () in 
   let data = List.fold_left (fun acc (lbl, str) -> acc @@ label lbl @@ asciiz str) nop !strings in
+  
   { text = text @@ printers; data = data }
--- typechecker.ml
+++ typechecker.ml
@@ -8,7 +8,7 @@ let string_of_typ = function
 
 let check loc expected actual =
   if expected <> actual then
-    error loc (Printf.sprintf "expected %s, got %s" (string_of_typ expected) (string_of_typ actual))
+    error loc ("type mismatch: expected " ^ (string_of_typ expected) ^ " but got " ^ (string_of_typ actual))
 
 module StringMap = Map.Make(String)
 
@@ -25,7 +25,7 @@ type env = {
 let empty_env = { structs = StringMap.empty; funcs = StringMap.empty; vars = StringMap.empty; ret_ty = [] }
 
 let check_wf loc env = function
-  | TStruct s when not (StringMap.mem s env.structs) -> error loc ("unknown struct: " ^ s)
+  | TStruct s when not (StringMap.mem s env.structs) -> error loc ("structure not declared: " ^ s)
   | _ -> ()
 
 
@@ -41,52 +41,63 @@ let get_binop_sig = function
 let rec type_expr env e =
   match e.edesc with
   | Int _ -> TInt | Bool _ -> TBool | String _ -> TString
-  | Nil -> error e.eloc "nil needs context"
+  | Nil -> error e.eloc "nil cannot be used without context"
   | Var id ->
-      (try StringMap.find id.id env.vars with Not_found -> error id.loc ("undefined var: " ^ id.id))
+      (try StringMap.find id.id env.vars 
+       with Not_found -> error id.loc ("variable not found: " ^ id.id))
   | New s ->
-      if not (StringMap.mem s env.structs) then error e.eloc ("unknown struct: " ^ s);
+      if not (StringMap.mem s env.structs) then error e.eloc ("unknown struct type: " ^ s);
       TStruct s
   | Dot (e, id) ->
       (match type_expr env e with
        | TStruct s ->
            let fields = (StringMap.find s env.structs).fields in
-           (try StringMap.find id.id fields with Not_found -> error id.loc ("no field " ^ id.id))
-       | _ -> error e.eloc "dot access requires struct pointer")
+           (try StringMap.find id.id fields 
+            with Not_found -> error id.loc ("field " ^ id.id ^ " does not exist in struct " ^ s))
+       | _ -> error e.eloc "dot access requires a struct pointer")
   | Call (f, args) ->
-      let info = try StringMap.find f.id env.funcs with Not_found -> error f.loc ("unknown func " ^ f.id) in
+      let info = try StringMap.find f.id env.funcs 
+                 with Not_found -> error f.loc ("undefined function " ^ f.id) in
       check_args env f.loc info.params args;
-      (match info.rets with [t] -> t | _ -> error e.eloc "func call must return exactly one value in expr")
+      (match info.rets with 
+       | [t] -> t 
+       | _ -> error e.eloc "function call returns multiple values, cannot be used as expression")
   | Unop (op, e1) ->
       let t = type_expr env e1 in
-      (match op, t with Opp, TInt -> TInt | Not, TBool -> TBool | _ -> error e.eloc "unop type mismatch")
+      (match op, t with 
+       | Opp, TInt -> TInt 
+       | Not, TBool -> TBool 
+       | _ -> error e.eloc "invalid unary operation for this type")
   | Binop ((Eq | Neq), e1, e2) ->
       let t1, t2 = try type_expr env e1, type_expr env e2 with _ -> TInt, TInt in
-      if e1.edesc = Nil && e2.edesc = Nil then error e.eloc "nil == nil illegal";
-      if e1.edesc <> Nil && e2.edesc <> Nil && t1 <> t2 then error e.eloc "type mismatch in cmp";
+      if e1.edesc = Nil && e2.edesc = Nil then error e.eloc "cannot compare nil with nil";
+      if e1.edesc <> Nil && e2.edesc <> Nil && t1 <> t2 then error e.eloc "types do not match in comparison";
       TBool
   | Binop (op, e1, e2) ->
       let t_in, t_out = get_binop_sig op in
       check e1.eloc t_in (type_expr env e1);
       check e2.eloc t_in (type_expr env e2);
       t_out
-  | Print _ -> error e.eloc "Print is an instruction"
+  | Print _ -> error e.eloc "fmt.Print is a statement, not an expression"
 
 and check_args env loc expected args =
-  if List.length expected <> List.length args then error loc "arity mismatch";
+  if List.length expected <> List.length args then error loc "wrong number of arguments";
   List.iter2 (fun t arg ->
-    if arg.edesc = Nil then (match t with TStruct _ -> () | _ -> error arg.eloc "nil requires struct type")
+    if arg.edesc = Nil then 
+        (match t with TStruct _ -> () | _ -> error arg.eloc "nil is only valid for pointers")
     else check arg.eloc t (type_expr env arg)
   ) expected args
 
 (* PASSE 2 : INSTRUCTIONS *)
 
-let check_lvalue e = match e.edesc with Var _ | Dot _ -> () | _ -> error e.eloc "lvalue required"
+let check_lvalue e = match e.edesc with 
+  | Var _ | Dot _ -> () 
+  | _ -> error e.eloc "left-hand side must be a variable or field"
 
 let get_rhs_types env exprs =
   match exprs with
   | [{edesc = Call(f, args); _}] ->
-      let info = try StringMap.find f.id env.funcs with Not_found -> error f.loc "unknown func" in
+      let info = try StringMap.find f.id env.funcs with Not_found -> error f.loc "function not found" in
       check_args env f.loc info.params args;
       info.rets
   | _ ->
@@ -100,12 +111,12 @@ let rec check_instr env instr =
            List.iter (fun arg ->
              match arg.edesc with
              | Call (f, a) -> check_args env f.loc (StringMap.find f.id env.funcs).params a
-             | Nil -> error arg.eloc "cannot print nil"
+             | Nil -> error arg.eloc "cannot print nil directly"
              | _ -> ignore (type_expr env arg)) args
        | _ -> ignore (type_expr env e))
   | Set (lvl, el) ->
       let types = get_rhs_types env el in
-      if List.length lvl <> List.length types then error instr.iloc "assign arity mismatch";
+      if List.length lvl <> List.length types then error instr.iloc "assignment mismatch: count of variables and values differs";
       List.iter2 (fun lv t_ex ->
         check_lvalue lv;
         match lv.edesc with
@@ -114,26 +125,32 @@ let rec check_instr env instr =
       ) lvl types
   | Vars (ids, t_opt, seq) ->
       let distinct = List.sort_uniq String.compare (List.map (fun i -> i.id) ids) in
-      if List.length distinct <> List.length ids then error instr.iloc "duplicate var names";
-      
+      if List.length distinct <> List.length ids then error instr.iloc "duplicate variable names in declaration";
       let new_vars = match t_opt, seq with
         | Some t, _ ->
             check_wf instr.iloc env t;
             List.fold_left (fun acc id -> StringMap.add id.id t acc) env.vars ids
         | None, {idesc=Set(_, el);_}::_ ->
             let types = get_rhs_types env el in
-            if List.length ids <> List.length types then error instr.iloc "init arity mismatch";
+            if List.length ids <> List.length types then error instr.iloc "variables and values count mismatch in init";
             List.fold_left2 (fun acc id t -> if id.id="_" then acc else StringMap.add id.id t acc) env.vars ids types
-        | _ -> error instr.iloc "invalid var decl"
+        | _ -> error instr.iloc "invalid variable declaration"
       in
       check_seq { env with vars = new_vars } seq
-  | Inc e | Dec e -> check_lvalue e; check e.eloc TInt (type_expr env e)
-  | If (c, b1, b2) -> check c.eloc TBool (type_expr env c); check_seq env b1; check_seq env b2
-  | For (c, b) -> check c.eloc TBool (type_expr env c); check_seq env b
+  | Inc e | Dec e -> 
+      check_lvalue e; 
+      check e.eloc TInt (type_expr env e)
+  | If (c, b1, b2) -> 
+      check c.eloc TBool (type_expr env c); 
+      check_seq env b1; 
+      check_seq env b2
+  | For (c, b) -> 
+      check c.eloc TBool (type_expr env c); 
+      check_seq env b
   | Block b -> check_seq env b
   | Return el ->
       let types = get_rhs_types env el in
-      if List.length types <> List.length env.ret_ty then error instr.iloc "return arity mismatch";
+      if List.length types <> List.length env.ret_ty then error instr.iloc "incorrect number of return values";
       List.iter2 (fun expected actual -> 
          if actual <> expected then check instr.iloc expected actual
       ) env.ret_ty types
@@ -154,21 +171,21 @@ let rec returns_all seq =
 let prog (_, decls) =
   let structs = List.fold_left (fun acc -> function
     | Struct s -> 
-        if StringMap.mem s.sname.id acc then error s.sname.loc "dup struct";
+        if StringMap.mem s.sname.id acc then error s.sname.loc ("struct " ^ s.sname.id ^ " is already defined");
         StringMap.add s.sname.id {fields=StringMap.empty; pos=s.sname.loc} acc
     | _ -> acc) StringMap.empty decls in
   let env = { empty_env with structs } in
 
   let env = List.fold_left (fun e -> function
     | Fun f ->
-        if StringMap.mem f.fname.id e.funcs then error f.fname.loc "dup func";
+        if StringMap.mem f.fname.id e.funcs then error f.fname.loc ("function " ^ f.fname.id ^ " already exists");
         List.iter (fun (_,t) -> check_wf f.fname.loc e t) f.params;
         List.iter (check_wf f.fname.loc e) f.return;
         { e with funcs = StringMap.add f.fname.id {params=List.map snd f.params; rets=f.return; pos=f.fname.loc} e.funcs }
     | Struct s ->
         let fields = List.fold_left (fun facc (id, t) ->
           check_wf id.loc e t;
-          if StringMap.mem id.id facc then error id.loc "dup field";
+          if StringMap.mem id.id facc then error id.loc ("duplicate field " ^ id.id);
           StringMap.add id.id t facc
         ) StringMap.empty s.fields in
         { e with structs = StringMap.add s.sname.id {fields; pos=s.sname.loc} e.structs }
@@ -179,9 +196,8 @@ let prog (_, decls) =
     | Fun f ->
         let vars = List.fold_left (fun acc (id, t) -> StringMap.add id.id t acc) StringMap.empty f.params in
         check_seq { env with vars; ret_ty = f.return } f.body;
-        if f.return <> [] && not (returns_all f.body) then error f.fname.loc "missing return"
+        if f.return <> [] && not (returns_all f.body) then error f.fname.loc "missing return statement at end of function body"
   ) decls;
-  
   if not (StringMap.mem "main" env.funcs) then 
-    error (Lexing.dummy_pos, Lexing.dummy_pos) "main required";
+    error (Lexing.dummy_pos, Lexing.dummy_pos) "missing main function";
   decls
